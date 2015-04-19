728335d304a6c1d3a517ba28ae8a6acc9b64572b
silverferret4 <silverferret4@gmail.com>
2015-04-19 12:55:19 -0500

    Modified asset_helper.php to be a little more user friendly (get_image_url is really helpful instead of having several smaller functions) and also corrected some path issues.
    Closes #57.

--- application/helpers/asset_helper.php
+++ application/helpers/asset_helper.php
@@ -3,10 +3,10 @@
 /**
  * Asset Helper
  *
- * @package	CodeIgniter
+ * @package		CodeIgniter
  * @subpackage	Helpers
  * @category	Helpers
- * @author	Ben Etten
+ * @author		Ben Etten
  */
 	
 // ------------------------------------------------------------------------
@@ -21,7 +21,7 @@ if ( ! function_exists('assets_url'))
 {
 	function assets_url()
 	{
-		return base_url().'assets/';
+		return base_url('assets');
 	}
 }
 	 
@@ -37,7 +37,7 @@ if ( ! function_exists('images_url'))
 {
 	function images_url()
 	{
-		return base_url().'assets/images/';
+		return base_url('assets/images');
 	}
 }
 
@@ -53,7 +53,7 @@ if ( ! function_exists('scripts_url'))
 {
 	function scripts_url()
 	{
-		return base_url().'assets/js/';
+		return base_url('assets/js');
 	}
 }
 
@@ -69,7 +69,7 @@ if ( ! function_exists('styles_url'))
 {
 	function styles_url()
 	{
-		return base_url().'assets/css/';
+		return base_url('assets/css');
 	}
 } 
 
@@ -79,32 +79,101 @@ if ( ! function_exists('styles_url'))
  * Returns the path to the css folder.
  *
  * @access	public
- * @return	url		Url to the css folder.
+ * @return	url		Url to the pdf folder.
  */
 if ( ! function_exists('pdfs_url'))
 {
 	function pdfs_url()
 	{
-		return base_url().'assets/pdf/';
+		return base_url('assets/pdf');
 	}
 } 
 
 // ------------------------------------------------------------------------
 
 /**
- * Returns a full path to blog image.
+ * Returns a full path to the blog version of the requested image.
  *
  * @access	public
  * @param	string	Name of the image.
- * @return	url		Url to the css folder.
+ * @return	url		Url to the requested image.
  */
-if ( ! function_exists('blog_url'))
+if ( ! function_exists('blog_image_url'))
 {
-	function blog_url($image_name)
+	function blog_image_url($image_name)
 	{
-		return base_url().'assets/images/'.$image_name.'/blog_full.jpg';
+		return base_url('assets/images/'.$image_name.'/blog_full.jpg');
+	}
+}
+
+// ------------------------------------------------------------------------
+
+/**
+ * Returns a full path to the large version of the requested image.
+ *
+ * @access	public
+ * @param	string	Name of the portfolio image.
+ * @param	string	Size of the portfolio image (large, medium, or small).
+ * @return	url		Url to the requested image.
+ */
+if ( ! function_exists('get_image_url'))
+{
+	function get_image_url($image_name, $image_size)
+	{
+		return base_url('assets/images/'.$image_name.'/'.$image_size.'.jpg');
 	}
 } 
+
+// ------------------------------------------------------------------------
+
+/**
+ * Returns a full path to the large version of the requested image.
+ *
+ * @access	public
+ * @param	string	Name of the image.
+ * @return	url		Url to the requested image.
+ */
+if ( ! function_exists('large_image_url'))
+{
+	function large_image_url($image_name)
+	{
+		return base_url('assets/images/'.$image_name.'/large.jpg');
+	}
+} 
+
+// ------------------------------------------------------------------------
+
+/**
+ * Returns a full path to the medium version of the requested image.
+ *
+ * @access	public
+ * @param	string	Name of the image.
+ * @return	url		Url to the requested image.
+ */
+if ( ! function_exists('medium_image_url'))
+{
+	function medium_image_url($image_name)
+	{
+		return base_url('assets/images/'.$image_name.'/medium.jpg');
+	}
+}  
+
+// ------------------------------------------------------------------------
+
+/**
+ * Returns a full path to the small version of the requested image.
+ *
+ * @access	public
+ * @param	string	Name of the image.
+ * @return	url		Url to the requested image.
+ */
+if ( ! function_exists('small_image_url'))
+{
+	function small_image_url($image_name)
+	{
+		return base_url('assets/images/'.$image_name.'/small.jpg');
+	}
+}  
 
 
 /* End of file asset_helper.php */
