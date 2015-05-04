a378a2912d1005cb7971fde8b7ddbc7837a1ebf9
nicholas <nicholasdiesslin@gmail.com>
2015-05-03 21:04:13 -0500

    added images to portfolio page, currently background images on a tags

--- application/views/pages/portfolio.php
+++ application/views/pages/portfolio.php
@@ -1,29 +1,8 @@
 <header class="intro"></header><!-- not sure if this will be used yet -->
-
-<div id="mill_city_ruins" class="portfolio_item">
-	<a href="<?php echo site_url("portfolio/mill_city_ruins"); ?>">
-		<img src="<?php echo blog_image_url('mill_city_ruins'); ?>" alt="Mill City Ruins">
-	</a>
-</div>
-
-<div id="alcatraz" class="portfolio_item">
-	<a href="<?php echo site_url("portfolio/alcatraz"); ?>">
-		<img src="<?php echo blog_image_url('alcatraz'); ?>" alt="Alcatraz">
-	</a>
-</div>
-
-<div id="sioux_falls_mill" class="portfolio_item">
-	<a href="<?php echo site_url("portfolio/sioux_falls_mill"); ?>">
-		<img src="<?php echo blog_image_url('sioux_falls_mill'); ?>" alt="Sioux Falls Mill">
-	</a>
-</div>
-
-<div id="white_river_barn" class="portfolio_item">
-	<a href="<?php echo site_url("portfolio/white_river_barn"); ?>">
-		<img src="<?php echo blog_image_url('white_river_barn'); ?>" alt="White River Barn">
-	</a>
-</div>
-
-<div id="kobenhavn" class="portfolio_item">
-	<a href="<?php echo site_url("portfolio/kobenhavn"); ?>">Kobenhavn</a>
+<div class="container">
+	<a href="<?php echo site_url("portfolio/mill_city_ruins"); ?>" id="mill_city_ruins" class="portfolio-item"></a>
+	<a href="<?php echo site_url("portfolio/alcatraz"); ?>" id="alcatraz" class="portfolio-item"></a>
+	<a href="<?php echo site_url("portfolio/sioux_falls_mill"); ?>" id="sioux_falls_mill" class="portfolio-item"></a>
+	<a href="<?php echo site_url("portfolio/white_river_barn"); ?>" id="white_river_barn" class="portfolio-item"></a>
+	<a href="<?php echo site_url("portfolio/kobenhavn"); ?>" id="kobenhavn" class="portfolio-item"></a>
 </div>
